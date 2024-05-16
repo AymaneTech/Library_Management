7a087fc92578c7751d27971727889745594ac7b6
geek <elmainiaymane03@gmail.com>
2024-05-16 20:27:45 +0100

    add the mockito librarry to write an efficient code and create a test class for db connection

--- src/main/java/yc/geek/database/DBConnection.java
+++ src/main/java/yc/geek/database/DBConnection.java
@@ -1,45 +1,46 @@
 package yc.geek.database;
 
 import yc.geek.Utils.Print;
+
 import java.sql.*;
 
 public class DBConnection {
 
-    private static DBConnection instance = null;
-    private Connection connection = null;
+     private static DBConnection instance = null;
+     private Connection connection = null;
 
-    private DBConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/library_management";
-        String username = "postgres";
-        String password = "admin";
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            Print.log(e.getMessage());
-        }
-    }
+     private DBConnection() throws SQLException {
+          String url = "jdbc:postgresql://localhost:5432/library_management";
+          String username = "postgres";
+          String password = "admin";
+          try {
+               connection = DriverManager.getConnection(url, username, password);
+          } catch (SQLException e) {
+               Print.log(e.getMessage());
+          }
+     }
 
-    public Connection getConnection() {
-        return connection;
-    }
+     public Connection getConnection() {
+          return connection;
+     }
 
-    public static DBConnection getInstance() throws SQLException {
-        if (instance == null || !instance.connection.isClosed()) {
-            instance = new DBConnection();
-        }
-        return instance;
-    }
+     public static DBConnection getInstance() throws SQLException {
+          if (instance == null || !instance.connection.isClosed()) {
+               instance = new DBConnection();
+          }
+          return instance;
+     }
 
-    public static boolean closeConnection() {
-        if (instance !=null){
-            try {
-                instance.getConnection().close();
-                instance = null;
-                return true;
-            } catch (SQLException e) {
-                Print.log(e.toString());
-            }
-        }
-        return false;
-    }
+     public static boolean closeConnection() {
+          if (instance != null) {
+               try {
+                    instance.getConnection().close();
+                    instance = null;
+                    return true;
+               } catch (SQLException e) {
+                    Print.log(e.toString());
+               }
+          }
+          return false;
+     }
 }
---
+++ src/test/java/yc/geek/database/DBConnectionTest.java
@@ -0,0 +1,20 @@
+package yc.geek.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBConnectionTest {
+
+     @Test
+     void getConnection() {
+     }
+
+     @Test
+     void getInstance() {
+     }
+
+     @Test
+     void closeConnection() {
+     }
+}
